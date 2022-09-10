f2d16e1919811accbe136682f4b58df9fe2f0ddc
Buğra Ateş <bugraatees@hotmail.com>
2022-09-11 01:28:32 +0300

    01.27

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -15,7 +15,16 @@ namespace MyApp // Note: actual namespace depends on the project name.
             {
                 Console.WriteLine(car.Description);
             }
-            carManager.Add(new Car { CarId = 4, BrandId = 2, ColorId = 33, DailyPrice =0m, ModelYear = "2018", Description = "Comfort" });
+            Car car1 = new Car();
+            car1.BrandId = 2;
+            car1.ColorId = 3;
+            car1.DailyPrice = 102.0m;
+            car1.ModelYear = "2005";
+            car1.Description = "ASDF";
+            carManager.Add(car1);
+            
+            carManager.Add(new Car { CarId = 1, BrandId = 1, ColorId = 23, DailyPrice = 344.5m, ModelYear = "2022", Description = "Fresh Comfort For Business Car" });
+            
         }
     }
 }
